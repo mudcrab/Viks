cc99cc8e712b14ad5ddd2aa67f9e1f89fab37359
Jevgeni Krais <beichu@gmail.com>
2012-11-16 15:08:04 +0200

    rectangle drawing

--- js/madness.js
+++ js/madness.js
@@ -1,14 +1,16 @@
 var Madness = Class.create({
     testent: null,
     stage: null,
+    input: null,
     initialize: function()
     {
 
-        this.testent = new PistonEntity({x: 10, y: 10}, {w: 32, h: 32}, 'player', 'Entity test');
+        this.testent = new PistonEntity({x: Math.floor($('gameDisplay').getWidth() / 2 - 16), y: Math.floor($('gameDisplay').getHeight() / 2 - 16)}, {w: 32, h: 32}, 'player', 'Entity test');
     },
     setup: function()
     {
         this.stage = new PistonStage();
+        this.input = new PistonInput();
         for(var i = 0; i < Math.floor($('gameDisplay').getWidth() / 32 + 1); i++)
         {
             for(var j = 0; j < Math.floor($('gameDisplay').getHeight() / 32 + 1); j++)
@@ -18,6 +20,10 @@ var Madness = Class.create({
             }
         }
         this.stage.addChild(this.testent);
+        this.testent.properties.speed = 2;
+        this.testent.rectSize = {w: 500, h: 300};
+        this.testent.rectPos = {x: Math.floor($('gameDisplay').getWidth() / 2 - 250), y: Math.floor($('gameDisplay').getHeight() / 2 - 150)};
+        this.testent.rectVisible = true;
     },
     draw: function()
     {
@@ -26,6 +32,22 @@ var Madness = Class.create({
     update: function()
     {
         $('fps').update(piston.fps);
+        if(this.input.keyDown('w'))
+        {
+            this.testent.move(0, -this.testent.properties.speed);
+        }
+        if(this.input.keyDown('s'))
+        {
+            this.testent.move(0, this.testent.properties.speed);
+        }
+        if(this.input.keyDown('a'))
+        {
+            this.testent.move(-this.testent.properties.speed, 0);
+        }
+        if(this.input.keyDown('d'))
+        {
+            this.testent.move(this.testent.properties.speed, 0);
+        }
     }
 });
 var piston = new PistonEngine('gameDisplay', Madness);
