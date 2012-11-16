b3f4cf472874ebe4e48ff6dc3a93793e01d9c2cc
Jevgeni Krais <beichu@gmail.com>
2012-11-16 12:36:16 +0200

    rendering working

--- js/madness.js
+++ js/madness.js
@@ -1,11 +1,23 @@
 var Madness = Class.create({
+    testent: null,
+    stage: null,
     initialize: function()
     {
 
+        this.testent = new PistonEntity({x: 10, y: 10}, {w: 32, h: 32}, 'player', 'Entity test');
     },
-    setup: function(stage)
+    setup: function()
     {
-
+        this.stage = new PistonStage();
+        for(var i = 0; i < Math.floor($('gameDisplay').getWidth() / 32 + 1); i++)
+        {
+            for(var j = 0; j < Math.floor($('gameDisplay').getHeight() / 32 + 1); j++)
+            {
+                var rand = Math.floor(Math.random() * 3) + 1;
+                this.stage.addChild(new PistonEntity({x: i * 32, y: j * 32}, {w: 32, h: 32}, 'grass' + rand));
+            }
+        }
+        this.stage.addChild(this.testent);
     },
     draw: function()
     {
@@ -17,4 +29,3 @@ var Madness = Class.create({
     }
 });
 var piston = new PistonEngine('gameDisplay', Madness);
-//var engine = new PistonEngine('gameDisplay', jQuery(window).width(), jQuery(window).height(), Madness, 'c2d', 8);
